cdced710b9f49a4fbfda7bdbf2e98bed0d52f6b6
jstewart983 <sarahstewart@Sarahs-MacBook-Air.local>
2015-03-17 22:23:26 -0500

    V1
    Header - Circles - Squares
    no data

---
+++ index.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Stewart Dashboard</title>
+		<link type="text/css" rel="stylesheet" href="css/stylesheet.css" />
+		<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+	</head>
+		<body class="text">
+		<div class="header">
+			<div id="header_text">
+				Stewart Dashboard
+			</div>
+		</div>
+		<div class="circle"><!--Diaper changes-->
+			<div class="data_title">
+				Title
+			</div>
+			<div class="data">
+				Data goes here
+			</div>
+		</div>
+		<div class="circle"><!--Bath-->
+			<div class="data_title">
+				Title
+			</div>
+			<div class="data">
+				Data goes here
+			</div>
+		</div>
+		<div class="circle"><!--Nap-->
+			<div class="data_title">
+				Title
+			</div>
+			<div class="data">
+				Data goes here
+			</div>
+		</div>	
+		<div class="circle"><!--Nursed-->
+			<div class="data_title">
+				Title
+			</div>
+			<div class="data">
+				Data goes here
+			</div>
+		</div>
+		<div class="circle"><!--Solids-->
+			<div class="data_title">
+				Title
+			</div>
+			<div class="data">
+				Data goes here
+			</div>
+		</div>
+		<div class="square"><!--Copper pee-->
+			<div class="data_title">
+				Title
+			</div>
+			<div class="data">
+				Data goes here
+			</div>
+		</div>
+		<div class="square"><!--Copper poop-->
+			<div class="data_title">
+				Title
+			</div>
+			<div class="data">
+				Data goes here
+			</div>
+		</div>
+		<div class="square"><!--Copper Exercised-->
+			<div class="data_title">
+				Title
+			</div>
+			<div class="data">
+				Data goes here
+			</div>
+		</div>
+		<div class="square"><!--Copper Crated-->
+			<div class="data_title">
+				Title
+			</div>
+			<div class="data">
+				Data goes here
+			</div>
+		</div>
+		</body>
+</html>
